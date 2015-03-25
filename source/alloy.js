3de99e0cd88e970959655835fe862f6dd6c19f2e
Chris Prucha <chris@sugarbox.io>
2015-03-25 02:42:49 -0700

    Remove global dependencies in NPM for now.

---
+++ source/alloy.js
@@ -0,0 +1,12 @@
+/*
+
+alloy.ts
+
+Created by Chris Prucha
+© 2015 Notion Labs, Inc
+
+*/
+var chalk = require("chalk");
+console.log(chalk.bgYellow("\n\n\t █████╗ ██╗     ██╗      ██████╗ ██╗   ██╗\n\t██╔══██╗██║     ██║     ██╔═══██╗╚██╗ ██╔╝\n\t███████║██║     ██║     ██║   ██║ ╚████╔╝\n\t██╔══██║██║     ██║     ██║   ██║  ╚██╔╝\n\t██║  ██║███████╗███████╗╚██████╔╝   ██║\n\t╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝    ╚═╝\n\tA Web Component Build System from Notion\n\n"));
+console.error(chalk.bgRed("Not implemented, exiting."));
+//# sourceMappingURL=alloy.js.map
