3cfbf0f06afcfdfbbddf7fac82970127ec905e71
Chris Prucha <chris@sugarbox.io>
2015-03-24 19:32:19 -0700

    Add alloy main file

---
+++ source/alloy.es6.js
@@ -0,0 +1,24 @@
+/*
+
+alloy.es6.js
+
+Created by Chris Prucha
+© 2015 Notion Labs, Inc
+
+*/
+
+var chalk  = require("chalk");
+
+console.log(chalk.bgYellow(`
+
+	 █████╗ ██╗     ██╗      ██████╗ ██╗   ██╗
+	██╔══██╗██║     ██║     ██╔═══██╗╚██╗ ██╔╝
+	███████║██║     ██║     ██║   ██║ ╚████╔╝
+	██╔══██║██║     ██║     ██║   ██║  ╚██╔╝
+	██║  ██║███████╗███████╗╚██████╔╝   ██║
+	╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝    ╚═╝
+	A Web Component Build System from Notion
+
+`));
+
+console.error(chalk.bgRed("Not implemented, exiting."));
