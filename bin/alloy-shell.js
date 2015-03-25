f0549c698a981e0a5e4dd92d3ab23c259d62b035
Chris Prucha <chris@sugarbox.io>
2015-03-25 02:18:03 -0700

    Fix global shell bin executable.

---
+++ bin/alloy-shell.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+/*
+
+ █████╗ ██╗     ██╗      ██████╗ ██╗   ██╗
+██╔══██╗██║     ██║     ██╔═══██╗╚██╗ ██╔╝
+███████║██║     ██║     ██║   ██║ ╚████╔╝
+██╔══██║██║     ██║     ██║   ██║  ╚██╔╝
+██║  ██║███████╗███████╗╚██████╔╝   ██║
+╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝    ╚═╝
+A Web Component Build System from Notion
+
+alloy-shell.js
+Execute Alloy from shell.
+
+Created by Chris Prucha
+© 2015 Notion Labs, Inc
+
+*/
+
+require("../build/alloy.js")
