763f263314b0c7553289400e3f5ab0ef7ceb77d3
Chris Prucha <chris@sugarbox.io>
2015-03-26 14:42:29 -0700

    Add vendor NPM.ts file so we can use ES6 module syntax.

---
+++ source/vendor/npm.ts
@@ -0,0 +1,14 @@
+/*
+
+npm.ts
+Use NPM modules with ES6 module syntax in TypeScript.
+
+NOTE(Chris): This file should be deprecated when TypeScript
+and TypeStrong no longer throw errors when importing NPM modules.
+
+Created by Chris Prucha
+© 2015 Notion Labs, Inc
+
+*/
+
+export let chalk = require("chalk")
