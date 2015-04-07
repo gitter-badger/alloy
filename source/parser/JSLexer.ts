330eba776a8b825a0fd6f64bfc809e24be5a1439
Chris Prucha <chris@sugarbox.io>
2015-04-07 14:16:07 -0700

    Refactor JSLexer

--- source/parser/JSLexer.ts
+++ source/parser/JSLexer.ts
@@ -20,68 +20,67 @@ export class JSLexer implements Lexer {
 	// Public
 	public generateTokens(code: string):Token[] {
 		let partialToken: string   = "";
-		let inName: boolean        = false;
-		let inString: boolean      = false;
+		let inName      : boolean  = false;
+		let inString    : boolean  = false;
 		let outputTokens: Token[]  = [];
 
 		for (let character of code) {
 			partialToken += character;
 
-			if (!inString && this.isTokenDelimiter(character)) {
+			if (this.isStringDelimiter(character)) {
 
-				if (inName && partialToken.length > 1) {
-					outputTokens.push(this.createToken("name", partialToken));
-					inName = false;
-				}
-
-				// Reset partial token and continue
-				partialToken = "";
-				continue
-
-			} else if (!inString && this.useToken(partialToken)) {
-				outputTokens.push(this.createToken("name", partialToken));
-				partialToken = "";
-				continue
-
-			} else if (!inString && this.isNumber(partialToken)) {
-
-				// Don't support numbers
-				throw new Error(`Unexpected token [number]: ${partialToken};`);
-
-			} else if (this.isStringDelimiter(character)) {
-
+				// Set inString if this is the first string delimiter.
 				if (!inString) {
 					inString = true;
 
+				// If we are in a string, create the string token and unset inString
 				} else {
 					outputTokens.push(this.createToken("string", partialToken));
 					inString = false;
 					partialToken = "";
 				}
-				continue
 
 			} else if (!inString) {
 
-				// Assume name
-				inName = true;
-			}
+				if (this.isTokenDelimiter(character)) {
 
+					// If we are in a name, create the name token and unset inName
+					// as long as the partial token is longer than the delimiter.
+					if (inName && partialToken.length > 1) {
+						outputTokens.push(this.createToken("name", partialToken));
+						inName = false;
+					}
+
+					// Ignore token delimiters
+					partialToken = "";
+					continue
+
+				} else if (this.useToken(partialToken)) {
+
+					// Output all declarative tokens
+					outputTokens.push(this.createToken("name", partialToken));
+					partialToken = "";
+					continue
+
+				} else if (this.isNumber(partialToken)) {
+
+					// Don't support numbers, throw an error
+					throw new Error(`Unexpected token [number]: ${partialToken};`);
+
+				} else {
+
+					// Assume the beginning of a name.
+					inName = true;
+				}
+			}
 		}
 
 		return outputTokens;
-
 	}
+
+	// Token Generation
 
 	// Helpers
-	private isTokenDelimiter(partialToken: string) {
-		for (let ignoredToken of this.tokens.token_delimiters) {
-			if (partialToken === ignoredToken) {
-				return true
-			}
-		}
-		return false
-	}
-
 	private useToken(partialToken: string): boolean {
 		let tokenTypes:any[] = [this.tokens.names, this.tokens.operators];
 		let tokenTypeIndex:number = 0;
@@ -92,6 +91,15 @@ export class JSLexer implements Lexer {
 				if (partialToken === matchToken) {
 					return true
 				}
+			}
+		}
+		return false
+	}
+
+	private isTokenDelimiter(partialToken: string) {
+		for (let ignoredToken of this.tokens.token_delimiters) {
+			if (partialToken === ignoredToken) {
+				return true
 			}
 		}
 		return false
