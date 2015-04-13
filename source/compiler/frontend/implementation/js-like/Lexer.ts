2b06c2daa81a236f15f823bed8261445b00f5415
Chris Prucha <chris@sugarbox.io>
2015-04-12 22:11:43 -0700

    More robust lexer

--- source/compiler/frontend/implementation/js-like/Lexer.ts
+++ source/compiler/frontend/implementation/js-like/Lexer.ts
@@ -6,7 +6,7 @@ import { default as isValidNameRegex } from "./match_es6_name";
 
 /*
 
-js-line/Lexer.ts
+js-like/Lexer.ts
 
 Created by Chris Prucha
 © 2015 Notion Labs, Inc
@@ -46,21 +46,41 @@ export class Lexer implements lexer {
 
 			} else if (!inString) {
 
+				if (inName && !this.isName(partialToken) || this.isMatchToken(partialToken)) {
+
+					// inName has ended
+					inName = false;
+
+					// Make sure it doesn't match a token and we are on a delimiter.
+					if (!this.isMatchToken(partialToken) && (this.isTokenDelimiter(character) || this.isMatchToken(character))) {
+
+						// Then the name ended.
+						let useToken = partialToken.slice(0, partialToken.length - 1);
+						outputTokens.push(this.createToken("name", useToken));
+						partialToken = partialToken.slice(partialToken.length - 1, partialToken.length);
+					}
+				}
+
 				if (this.isTokenDelimiter(character)) {
 
 					// If we are in a unknown, create the unknown token and unset inUnknown
 					// as long as the partial token is longer than the delimiter.
-					if ((inUnknown || inName) && partialToken.length > 1) {
+					if (inUnknown && partialToken.length > 1) {
 
 						// Slice off the delimiter
-						let useToken = partialToken.slice(0, partialToken.length - 1)
-						let tokenType =  inName && !inUnknown ? "name" : "unknown";
-						outputTokens.push(this.createToken(tokenType, useToken));
+						let useToken = partialToken.slice(0, partialToken.length - 1);
+						outputTokens.push(this.createToken("unknown", useToken));
 						inUnknown = false;
 						inName    = false;
+					} else {
+						outputTokens.push(this.createToken("delimiter", partialToken));
 					}
 
 					// Ignore token delimiters
+					partialToken = "";
+
+				} else if (this.isComment(partialToken)) {
+					outputTokens.push(this.createToken("comment", partialToken));
 					partialToken = "";
 
 				} else if (this.isMatchToken(partialToken)) {
@@ -71,13 +91,7 @@ export class Lexer implements lexer {
 					partialToken = "";
 
 				} else if (this.isName(partialToken)) {
-					inName = true
-
-				} else if (this.isNumber(partialToken)) {
-
-					// Don't support numbers, continue
-					// TODO(Chris): Implement proper number support to detect errors.
-					continue;
+					inName = true;
 
 				} else {
 
@@ -147,12 +161,17 @@ export class Lexer implements lexer {
 
 	}
 
-	private isName(partialToken: string): boolean {
-		return isValidNameRegex.test(partialToken);
+	private isComment(partialToken: string): boolean {
+		for (let delimiter of this.tokens.comment_delimiters) {
+			if (partialToken === delimiter) {
+				return true;
+			}
+		}
+		return false;
 	}
 
-	private isNumber(partialToken: string): boolean {
-		return !!parseInt(partialToken, 10);
+	private isName(partialToken: string): boolean {
+		return isValidNameRegex.test(partialToken);
 	}
 
 	private createToken(tokenType: string, tokenValue: string):token {
