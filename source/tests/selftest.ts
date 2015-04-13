bffc9a946753319d8bcf49fa1bc940f22a4337d4
Chris Prucha <chris@sugarbox.io>
2015-04-12 22:14:01 -0700

    Parser test

--- source/tests/selftest.ts
+++ source/tests/selftest.ts
@@ -1,37 +1,45 @@
 import { TOKENS } from "../compiler/frontend/implementation/js-like/TOKENS";
 import { Lexer }  from "../compiler/frontend/implementation/js-like/Lexer";
 import { Parser } from "../compiler/frontend/implementation/js-like/Parser";
+import * as util  from "util";
 
 export let selftest = ():void => {
 
-  // Simple Lexer test
-  console.log(new Lexer(TOKENS).generateTokens(`
-    import { Bar } from "./bar";
-    import * as foo from "./foo";
-    import Test from "./test";
+  // Parser test
+  console.log(util.inspect(new Parser().parse(`
+    // Destructuring
+    import { x } from "./Foo";
+    import { x, y } from "./Foo";
+
+
+    // Default
+    import y, { x } from "./Foo";
+    import y from "./Foo";
+    import { default as y } from "./Foo";
+
+    // Aliasing
+    import * as y from "./Foo";
+    import Ff2_oo as Y from "./Foo";
+
+    // Destructuring + Aliasing
+    import { x as y, o } from "./Foo";
 
     function Foo () {
-      var a = "ap{ple";
+      var a = "apple";
       var b = 23423;
       return a + b;
     }
 
+    export var u = false;
+
+    export let x = 2.3;
+
     console.log(Bar, foo, Test);
     console.log(Foo());
-  `));
 
-  // console.log(new Parser().parse(`
-  //   import { Bar } from "./bar";
-  //   import * as foo from "./foo";
-  //   import Test from "./test";
-  //
-  //   function Foo () {
-  //     var a = "apple";
-  //     var b = 23423;
-  //     return a + b;
-  //   }
-  //
-  //   console.log(Bar, foo, Test);
-  //   console.log(Foo());
-  // `));
+  `), {
+    depth : null,
+    colors: true
+  }));
+
 }
